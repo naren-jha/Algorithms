89afdd7079a9899ae75bbfd724ceb52b523ad636
nkjha135 <nkjha2075@gmail.com>
2020-08-05 04:46:58 +0530

    refactoring and bug fixes

--- src/basic/queue/usingarray/QueueTest.java
+++ src/basic/queue/usingarray/QueueTest.java
@@ -1,72 +1,70 @@
 package basic.queue.usingarray;
 
+import java.util.Arrays;
+
 /**
+ * Implementation of Queue data structure using array
+ * 
  * @author Narendra Jha, njha.sde@gmail.com
  *
- * Queue using array
  */
 class Queue {
     private int[] a;
     private int front;
     private int rear;
-    private int N = 100;
     
     public Queue() {
+        this(16);
+    }
+    
+    public Queue(int n) {
         front = rear = -1;
-        a = new int[N];
+        a = new int[n];
     }
     
     public boolean isEmpty() {
         return (front == -1 && rear == -1);
     }
     
-    public void enqueue(int value) {
-        if ((rear + 1) % N == front) {
-            // when queue is full
-            throw new IllegalStateException("Queue is full");
+    public void offer(int value) {
+        int n = a.length;
+        if ((rear + 1) % n == front) {
+            //throw new IllegalStateException("Queue is full");
+            n *= 2;
+            a = Arrays.copyOf(a, n);
         }
-        else if (isEmpty()) {
-            // when queue is empty
+        
+        if (isEmpty())
             front = rear = 0;
-        }
-        else {
-            rear = (rear + 1) % N;
-        }
+        else
+            rear = (rear + 1) % n;
         a[rear] = value;
     }
     
-    public int dequeue() {
-        int value;
-        if (isEmpty()) {
-            // when queue is empty
-            throw new IllegalStateException("Queue is empty");
-        }
-        else if (front == rear) {
-            // when queue has only one element
-            // in that case, dequeue operation
-            // should make queue empty
-            value = a[front];
+    public int poll() {
+        if (isEmpty())
+            throw new IllegalStateException("empty queue");
+        
+        int n = a.length;
+        int value= a[front];
+        if (front == rear)
             front = rear = -1;
-        }
-        else {
-            value = a[front];
-            front = (front + 1) % N;
-        }
+        else
+            front = (front + 1) % n;
         return value;
     }
     
-    public int front() {
-        if (isEmpty()) {
-            // when queue is empty
-            throw new IllegalStateException("Queue is empty");
-        }
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException("empty queue");
         return a[front];
     }
     
     @Override
     public String toString() {
+        int n = a.length;
         StringBuilder result = new StringBuilder("[");
-        for (int i = front; i != rear; i = (i + 1) % N)
+        for (int i = front; i != rear; i = (i + 1) % n)
             result.append(a[i]).append(", ");
         if (rear != -1)
             result.append(a[rear]);
@@ -79,12 +77,12 @@ class Queue {
 public class QueueTest {
 
     public static void main(String[] args) {
-        Queue q = new Queue();
-        q.enqueue(2);q.enqueue(4);q.enqueue(6);q.enqueue(8);
+        Queue q = new Queue(3);
+        q.offer(2);q.offer(4);q.offer(6);q.offer(8);
         System.out.println(q); // [2, 4, 6, 8]
-        System.out.println(q.dequeue()); // 2
+        System.out.println(q.poll()); // 2
         System.out.println(q); // [4, 6, 8]
-        System.out.println(q.front()); // 4
+        System.out.println(q.peek()); // 4
         System.out.println(q); // [4, 6, 8]
     }
 
