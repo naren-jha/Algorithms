311d86989fc0c09aa73caf5a4745a7e0d2430a83
nkjha135 <nkjha2075@gmail.com>
2020-08-17 05:24:43 +0530

    refactoring and bug fixes..

--- src/graph/FindCycleUnionFind.java
+++ src/graph/FindCycleUnionFind.java
@@ -5,8 +5,10 @@ import java.util.LinkedList;
 import basic.unionfind.UnionFind;
 
 /**
- * Implementation of algorithm to find cycle in a given directed graph
+ * Implementation of algorithm to find cycle in a given undirected graph
  * using Union Find data structure
+ * 
+ * Note: Using Union Find, we cannot find cycle in a directed graph
  *
  * @author Narendra Jha, njha.sde@gmail.com
  *
@@ -33,19 +35,22 @@ public class FindCycleUnionFind extends Graph {
     }
     
     public static void main(String[] args) {
-        // Example - directed graph
-        FindCycleUnionFind graph = new FindCycleUnionFind(5);
+        FindCycleUnionFind g1 = new FindCycleUnionFind(5); 
+        g1.addEdge(1, 0, false); 
+        g1.addEdge(0, 2, false); 
+        g1.addEdge(2, 1, false); 
+        g1.addEdge(0, 3, false); 
+        g1.addEdge(3, 4, false);
+        System.out.println(g1.hasCycle()); // true
+  
+        FindCycleUnionFind g2 = new FindCycleUnionFind(3); 
+        g2.addEdge(0, 1, false); 
+        g2.addEdge(1, 2, false); 
+        System.out.println(g2.hasCycle()); // false
         
-        graph.addEdge(0, 1, false);
-        graph.addEdge(0, 3, false);
-        graph.addEdge(1, 2, false);
-        graph.addEdge(3, 4, false);
-        graph.addEdge(4, 0, false);
-        
-        System.out.println(graph.hasCycle()); // true
-        
-        graph.removeEdge(4, 0, false);
-        System.out.println(graph.hasCycle()); // false
+        // Every directed edge here is treated as an undirected edge
+        // that means, we don't add two edges u->v and v->u
+        // there is just one u->v which is treated as bidirectional
     }
 
 }
