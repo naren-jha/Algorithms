fb3744219cc75803c3956a31924a35baad7a78e9
nkjha135 <nkjha2075@gmail.com>
2020-12-19 09:22:22 +0530

    refactoring and bug fixes

--- src/dynamicprogramming/basic/CoverDistance.java
+++ src/dynamicprogramming/basic/CoverDistance.java
@@ -6,12 +6,12 @@ package dynamicprogramming.basic;
 public class CoverDistance {
 
     class SimpleRecursiveSolution {
-        // T(n): Exp
+        // T(n): O(3^n), S(n): O(n)
         public int countWays(int n) {
-            if (n == 0)
-                return 1;
-            if (n < 0)
-                return 0;
+            if (n == 0) return 1;
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            
             return countWays(n-3) + countWays(n-2) + countWays(n-1);
         }
     }
@@ -20,19 +20,32 @@ public class CoverDistance {
         // bottom-up tabulation
         // T(n): O(n), S(n): O(n)
         public int countWays(int n) {
-            int[] res = new int[n+1];
+            if (n <= 1) return 1; // edge case
             
-            res[0] = 1;
-            res[1] = 1;
-            res[2] = 2;
+            int[] dp = new int[n+1];
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 2;
             
             for (int i = 3; i <= n; i++)
-                res[i] = res[i-3] + res[i-2] + res[i-1];
+                dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
             
-            return res[n];
+            return dp[n];
         }
         
         // we can space optimize above solution to O(1) by using 3 variables
+        // T(n): O(n), S(n): O(1)
+        public int countWaysSO(int n) {
+            if (n <= 1) return 1; // edge case
+            
+            int t0 = 1, t1 = 1, t2 = 2, t;
+            for (int i = 3; i <= n; i++) {
+                t = t0 + t1 + t2;
+                t0 = t1; t1 = t2; t2 = t;
+            }
+            
+            return t2;
+        }
     }
     
     public static void main(String[] args) {
@@ -40,5 +53,6 @@ public class CoverDistance {
         CoverDistance obj = new CoverDistance();
         System.out.println(obj.new SimpleRecursiveSolution().countWays(n)); // 7
         System.out.println(obj.new DPSolution().countWays(n)); // 7
+        System.out.println(obj.new DPSolution().countWaysSO(n)); // 7
     }
 }
