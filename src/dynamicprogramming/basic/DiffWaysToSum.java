a3b3253994462406d5ebfe619634b31f3521236e
nkjha135 <nkjha2075@gmail.com>
2020-12-19 13:57:16 +0530

    refactoring and bug fixes

--- src/dynamicprogramming/basic/DiffWaysToSum.java
+++ src/dynamicprogramming/basic/DiffWaysToSum.java
@@ -1,5 +1,7 @@
 package dynamicprogramming.basic;
 
+import java.util.Arrays;
+
 // https://www.geeksforgeeks.org/different-ways-sum-n-using-numbers-greater-equal-m/
 
 public class DiffWaysToSum {
@@ -14,27 +16,11 @@ public class DiffWaysToSum {
     
     class SimpleRecursiveSolution {
         // T(n): Exp
-        /*public int countWays(int n, int m) {
-            if (n == 0)
-                return 1;
-            if (n < 0)
-                return 0;
-            if (m > n)
-                return 0;
+        public int countWays(int n, int m) {
+            if (n == 0) return 1;
+            if (m > n) return 0;
             
             return countWays(n-m, m) + countWays(n, m+1);
-        }*/
-        
-        public int countWays(int n, int m) {
-            if (n == 0)
-                return 1;
-            
-            int count = 0;
-            if (n >= m)
-                count += countWays(n-m, m);
-            if (m < n)
-                count += countWays(n, m+1);
-            return count;
         }
     }
     
@@ -42,22 +28,16 @@ public class DiffWaysToSum {
         // Bottom-up tabulation
         // T(n): O(n*(n-m)), S(n): O(mn)
         public int countWays(int n, int m) {
-            int[][] res = new int[n+1][n+1];
+            int[][] dp = new int[n+1][n+1];
+            //Arrays.fill(dp[0], 1);
             
-            for (int i = 0; i <= n; i++) {
-                for (int j = n; j >= m; j--) {
-                    if (i == 0) { // for n = 0 case
-                        res[i][j] = 1;
-                        continue;
-                    }
-                    
-                    if (i >= j)
-                        res[i][j] += res[i-j][j];
-                    if (j < n)
-                        res[i][j] += res[i][j+1];
-                }
+            for (int i = 1; i <= n; i++) {
+                dp[i][i] = 1;
+                for (int j = i-1; j >= m; j--)
+                    dp[i][j] = dp[i-j][j] + dp[i][j+1];
             }
-            return res[n][m];
+            
+            return dp[n][m];
         }
         
         // We should be able to space optimize above solution
--- src/dynamicprogramming/intermediate/Knapsack.java
+++ src/dynamicprogramming/intermediate/Knapsack.java
@@ -1,5 +1,6 @@
 package dynamicprogramming.intermediate;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 // https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
@@ -10,16 +11,12 @@ public class Knapsack {
     class SimpleRecursiveSolution {
         // T(n): Exp
         public int knapsack(int[] wt, int[] val, int n, int w) {
-            if (n == 0)
-                return 0;
-            if (w == 0)
-                return 0;
+            if (n == 0 || w == 0) return 0;
             
-            if (w < wt[n-1]) // when last element cannot be included
-                return knapsack(wt, val, n-1, w);
-            else
-                return Math.max(knapsack(wt, val, n-1, w - wt[n-1]) + val[n-1], 
-                            knapsack(wt, val, n-1, w));
+            int sum = knapsack(wt, val, n-1, w);
+            if (w >= wt[n-1]) // when last element can be included
+                sum = Math.max(sum, knapsack(wt, val, n-1, w - wt[n-1]) + val[n-1]) ;
+            return sum;
         }
     }
     
@@ -29,15 +26,15 @@ public class Knapsack {
         public int knapsack(int[] wt, int[] val, int n, int w) {
             int[][] k = new int[n+1][w+1];
             
-            for (int i = 0; i <= n; i++) {
-                for (int j = 0; j <= w; j++) {
-                    if (i == 0 || j == 0)
-                        k[i][j] = 0;
-                    else if (j < wt[i-1]) // when last element cannot be included
-                        k[i][j] = k[i-1][j];
-                    else
-                        k[i][j] = Math.max(k[i-1][j-wt[i-1]] + val[i-1], 
-                                            k[i-1][j]);
+            for (int i = 0; i <= n; i++) k[i][0] = 0;
+            for (int j = 0; j <= w; j++) k[0][j] = 0;
+            
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= w; j++) {
+                    k[i][j] = k[i-1][j];
+                    if (j >= wt[i-1]) // when last element can be included
+                        k[i][j] = Math.max(k[i][j], 
+                                            k[i-1][j-wt[i-1]] + val[i-1]);
                 }
             }
             
@@ -53,15 +50,15 @@ public class Knapsack {
              * would get overwritten if we use 1-d array
              */
             
-            for (int i = 0; i <= n; i++) {
-                for (int j = 0; j <= w; j++) {
-                    if (i == 0 || j == 0)
-                        k[i % 2][j] = 0;
-                    else if (j < wt[i-1]) // when last element cannot be included
-                        k[i % 2][j] = k[(i-1) % 2][j];
-                    else
-                        k[i % 2][j] = Math.max(k[(i-1) % 2][j-wt[i-1]] + val[i-1], 
-                                            k[(i-1) % 2][j]);
+            for (int j = 0; j <= w; j++) k[0][j] = 0;
+            k[1][0] = 0;
+            
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= w; j++) {
+                    k[i % 2][j] = k[(i-1) % 2][j];
+                    if (j >= wt[i-1]) // when last element can be included
+                        k[i % 2][j] = Math.max(k[i % 2][j], 
+                                                k[(i-1) % 2][j-wt[i-1]] + val[i-1]);
                 }
             }
             
@@ -72,15 +69,15 @@ public class Knapsack {
         public void printKnapsack(int[] wt, int[] val, int n, int w) {
             int[][] k = new int[n+1][w+1];
             
-            for (int i = 0; i <= n; i++) {
-                for (int j = 0; j <= w; j++) {
-                    if (i == 0 || j == 0)
-                        k[i][j] = 0;
-                    else if (j < wt[i-1]) // when last element cannot be included
-                        k[i][j] = k[i-1][j];
-                    else
-                        k[i][j] = Math.max(k[i-1][j-wt[i-1]] + val[i-1], 
-                                            k[i-1][j]);
+            for (int i = 0; i <= n; i++) k[i][0] = 0;
+            for (int j = 0; j <= w; j++) k[0][j] = 0;
+            
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= w; j++) {
+                    k[i][j] = k[i-1][j];
+                    if (j >= wt[i-1]) // when last element can be included
+                        k[i][j] = Math.max(k[i][j], 
+                                            k[i-1][j-wt[i-1]] + val[i-1]);
                 }
             }
             
@@ -103,9 +100,8 @@ public class Knapsack {
                 i--;
             }
             
-            while (!res.isEmpty()) {
+            while (!res.isEmpty())
                 System.out.print(res.pop() + " ");
-            }
             System.out.println(); // new line after result
         }
         
@@ -118,15 +114,15 @@ public class Knapsack {
         public int knapsack(int[] wt, int[] val, int n, int w) {
             int[][] k = new int[n+1][w+1];
             
-            for (int i = 0; i <= n; i++) {
-                for (int j = 0; j <= w; j++) {
-                    if (i == 0 || j == 0)
-                        k[i][j] = 0;
-                    else if (j < wt[i-1]) // when last element cannot be included
-                        k[i][j] = k[i-1][j];
-                    else
-                        k[i][j] = Math.max(k[i][j-wt[i-1]] + val[i-1], 
-                                            k[i-1][j]);
+            for (int i = 0; i <= n; i++) k[i][0] = 0;
+            for (int j = 0; j <= w; j++) k[0][j] = 0;
+            
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= w; j++) {
+                    k[i][j] = k[i-1][j];
+                    if (j >= wt[i-1]) // when last element can be included
+                        k[i][j] = Math.max(k[i][j], 
+                                            k[i][j-wt[i-1]] + val[i-1]);
                 }
             }
             
@@ -140,17 +136,48 @@ public class Knapsack {
              * We can use 1-d array here because we need k[i][j-wt[i-1]]
              * which is same row's some previous column value
              */
+            Arrays.fill(k, 0);
             
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j <= w; j++) {
-                    if (i == 0 || j == 0)
-                        k[j] = 0; // redundant in Java
-                    else if (j >= wt[i]) // when last element can be included
-                        k[j] = Math.max(k[j-wt[i]] + val[i], k[j]);
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= w; j++) {
+                    if (j >= wt[i-1]) // when last element can be included
+                        k[j] = Math.max(k[j], 
+                                        k[j-wt[i-1]] + val[i-1]);
                 }
             }
             
             return k[w];
+        }
+        
+        public void printKnapsack(int[] wt, int[] val, int n, int w) {
+            int[][] k = new int[n+1][w+1];
+            
+            for (int i = 0; i <= n; i++) k[i][0] = 0;
+            for (int j = 0; j <= w; j++) k[0][j] = 0;
+            
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= w; j++) {
+                    k[i][j] = k[i-1][j];
+                    if (j >= wt[i-1]) // when last element can be included
+                        k[i][j] = Math.max(k[i][j], 
+                                            k[i][j-wt[i-1]] + val[i-1]);
+                }
+            }
+            
+            // return k[n][w];
+            int i = n, j = w;
+            Stack<Integer> res = new Stack<Integer>();
+            while (i > 0 && k[i][j] > 0) {
+                while (k[i][j] != k[i-1][j]) {
+                    res.push(wt[i-1]);
+                    j = j - wt[i-1];
+                }
+                i--;
+            }
+            
+            while (!res.isEmpty())
+                System.out.print(res.pop() + " ");
+            System.out.println(); // new line after result
         }
     }
     
@@ -167,12 +194,14 @@ public class Knapsack {
         k.new DPSolution().printKnapsack(wt, val, n, w); // 3 4 
         
         // Unbounded knapsack test
+        System.out.println("===== Unbounded knapsack =====");
         int[] wt2 = {1, 3, 4, 5};
         int[] val2 = {10, 40, 50, 70};
         int w2 = 8;
         int n2 = 4;
         System.out.println(k.new UnboundedKnapsack().knapsack(wt2, val2, n2, w2)); // 110
         System.out.println(k.new UnboundedKnapsack().knapsackSpaceOptimized(wt2, val2, n2, w2)); // 110
+        k.new UnboundedKnapsack().printKnapsack(wt2, val2, n2, w2); // 3 5
         
         int[] wt3 = {5, 10, 15};
         int[] val3 = {10, 30, 20, 70};
@@ -180,5 +209,6 @@ public class Knapsack {
         int n3 = 3;
         System.out.println(k.new UnboundedKnapsack().knapsack(wt3, val3, n3, w3)); // 300
         System.out.println(k.new UnboundedKnapsack().knapsackSpaceOptimized(wt3, val3, n3, w3)); // 300
+        k.new UnboundedKnapsack().printKnapsack(wt3, val3, n3, w3); // 10 10 10 10 10 10 10 10 10 10
     }
 }
