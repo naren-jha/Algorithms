514307893ce6a6b83bb1488f3d07b2f0bc5ffe2f
nkjha135 <nkjha2075@gmail.com>
2021-01-05 18:43:50 +0530

    refactoring and bug fixes

--- src/dynamicprogramming/intermediate/LongestPalindromicSubstring.java
+++ src/dynamicprogramming/intermediate/LongestPalindromicSubstring.java
@@ -18,7 +18,7 @@ public class LongestPalindromicSubstring {
     public int longestPalindromeSubstring(String s) {
         int n = s.length();
         boolean[][] dp = new boolean[n][n];
-        int maxLen = 1, start = 0;
+        int maxLen = 1, si = 0;
         
         // len 1 substrings
         for (int i = 0; i < n; ++i) dp[i][i] = true;
@@ -28,7 +28,7 @@ public class LongestPalindromicSubstring {
             dp[i][i+1] = s.charAt(i) == s.charAt(i+1);
             if (dp[i][i+1]) {
                 maxLen = 2;
-                start = i;
+                si = i;
             }
         }
         
@@ -38,19 +38,53 @@ public class LongestPalindromicSubstring {
                 dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i+1][j-1]);
                 if (dp[i][j] && len > maxLen) {
                     maxLen = len;
-                    start = i;
+                    si = i;
                 }
             }
         }
         
-        System.out.println("Longest Palindrome Substring is: " + s.substring(start, start+maxLen));
+        System.out.println("Longest Palindrome Substring is: " + s.substring(si, si + maxLen));
         return maxLen;
     }
     
-    /*
-     * There is a O(n^2) time and O(1) space solution discussed at: 
-     * https://www.geeksforgeeks.org/longest-palindromic-substring-set-2/
-     */
+    // We can also solve this problem in O(n^2) time and O(1) space. See solution below.
+    // The idea is to choose a center for the palindrome and expand as long as possible.
+    // Center of palindrome will be a character, if the length of palindrome is odd (example: abcba).
+    // Center of palindrome will be two characters (or space between them), if the length of palindrome is even (example: abba).
+    // so we try all possiblity for even and odd length palindromes
+    public int longestPalindromeSubstr(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        
+        int maxLen = 1, si = 0;
+        for (int i = 0; i < n; ++i) {
+            int len1 = expand(s, i, i); // odd length palindrome
+            int len2 = expand(s, i, i+1); // even length palindrome
+            int len = Math.max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                si = i - (len-1) / 2;
+            }
+        }
+        
+        
+        System.out.println("Longest Palindrome Substring is: " + s.substring(si, si + maxLen));
+        return maxLen;
+    }
+    
+    int expand(String s, int i, int j) {
+        int n = s.length();
+        while (i >= 0 && j < n && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
+        }
+        
+        // above while loop will fail either because index went out of bounds or characters didn't match
+        // so we'll have to contract one position left and right to get valid length
+        i++; j--;
+        
+        return j-i+1;
+    }
     
     /*
      * This problem also has a linear time solution using Manacher’s Algorithm
@@ -61,6 +95,9 @@ public class LongestPalindromicSubstring {
         LongestPalindromicSubstring obj = new LongestPalindromicSubstring();
         
         System.out.println(obj.longestPalindromeSubstring("aacabdkacaa")); // 3
+        // Longest Palindrome Substring is: aca
+        
+        System.out.println(obj.longestPalindromeSubstr("aacabdkacaa")); // 3
         // Longest Palindrome Substring is: aca
     }
 }
