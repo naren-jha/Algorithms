0ddb9c77059a3f9c447aaf51961faa731f730ff8
nkjha135 <nkjha2075@gmail.com>
2021-01-05 17:34:53 +0530

    refactoring and bug fixes

--- src/dynamicprogramming/intermediate/LongestPalindromicSubstring.java
+++ src/dynamicprogramming/intermediate/LongestPalindromicSubstring.java
@@ -1,10 +1,17 @@
 package dynamicprogramming.intermediate;
 
-import java.util.Arrays;
-
 // https://www.geeksforgeeks.org/longest-palindrome-substring-set-1/
 
 public class LongestPalindromicSubstring {
+    
+    // If we apply brute-force approach to check. Then to check all the possible substring, it'll take O(n^2)
+    // and then to check if each of those substring is a palindrome or not, it'll take another O(n). So total O(n^3).
+    // But we are doing some repetitive work in this process. When we check for s[i to j], we are also checking for 
+    // s[i+1 to j-1], and then later we solve for s[i+1 to j-1] again (subproblem for s[i+1 to j-1] itself
+    // has many other overlapping subproblems). So we'll have to use DP to solve this efficiently.
+    // We can solve the subproblems starting from smaller length and going towards higher length.
+    
+    // d[i][j] stores true, if s[i...j] is a palindrome, else it stores false.
 
     // bottom-up tabulation
     // T(n): O(n^2), S(n): O(n^2)
@@ -26,8 +33,8 @@ public class LongestPalindromicSubstring {
         }
         
         for (int len = 3; len <= n; ++len) {
-            for (int i = 0; i <= n - len; i++){
-                int j = i + len - 1;
+            for (int i = 0; i < n - (len-1); i++){
+                int j = i + (len - 1);
                 dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i+1][j-1]);
                 if (dp[i][j] && len > maxLen) {
                     maxLen = len;
