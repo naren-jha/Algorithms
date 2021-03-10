374e201dff4f658238e675cc2f64e8b35babbb08
Narendra Jha <njha.sde@gmail.com>
2021-03-10 23:21:01 +0530

    refactors BinaryIndexedTree

--- src/advance_ds/BinaryIndexedTree.java
+++ src/advance_ds/BinaryIndexedTree.java
@@ -5,80 +5,49 @@ package advance_ds;
  * 
  * Implementation of Binary Indexed Tree / Fenwick Tree data structure
  */
+
 public class BinaryIndexedTree {
     
-    // Starting from index+1 keep updating affected nodes of BIT
-    // till next node is not outside the range of BIT
-    public void update(int[] bit, int val, int index) {
-        index = index + 1;
-        while (index < bit.length) {
-            bit[index] += val;
-            index = getNext(index);
+    private int[] bit;
+    
+    BinaryIndexedTree(int[] input) {
+        bit = new int[input.length + 1];
+        
+        for (int i = 0; i < input.length; i++) {
+            update(input[i], i);
         }
     }
+
+    public void update(int val, int index) {
+        for (int i = index+1; i < bit.length; i += (i & -i))
+            bit[i] += val;
+    }
     
-    // Starting from index+1 keep adding value till you reach node 0
-    public int sum(int[] bit, int index) {
-        index = index + 1;
+    public int sum(int index) {
         int sum = 0;
-        while (index > 0) {
-            sum += bit[index];
-            index = getParent(index);
-        }
+        for (int i = index+1; i > 0; i -= (i & -i)) 
+            sum += bit[i];
         return sum;
     }
-    
-    // Creating BIT is like updating the tree for every element of input array
-    public int[] createTree(int[] input) {
-        int[] bit = new int[input.length + 1];
-        for (int i = 0; i < input.length; i++) {
-            update(bit, input[i], i);
-        }
-        return bit;
-    }
-
-    /**
-     * To get parent
-     * 1) Get 2's complement to get minus of index number
-     * 2) AND it with original index number
-     * 3) Subtract result from original index number
-     */
-    private int getParent(int index) {
-        return index - (index & -index);
-    }
-
-    /**
-     * To get next
-     * 1) Get 2's complement to get minus of index number
-     * 2) AND it with original index number
-     * 3) Add result to original index number
-     */
-    private int getNext(int index) {
-        return index + (index & -index);
-    }
-
-
 
     public static void main(String[] args) {
         int[] input = {1, 2, 3, 4, 5, 6, 7};
-        BinaryIndexedTree tree = new BinaryIndexedTree();
-        int[] bit = tree.createTree(input);
-        System.out.println(tree.sum(bit, 0)); // 1
-        System.out.println(tree.sum(bit, 1)); // 3
-        System.out.println(tree.sum(bit, 2)); // 6
-        System.out.println(tree.sum(bit, 3)); // 10
-        System.out.println(tree.sum(bit, 4)); // 15
-        System.out.println(tree.sum(bit, 5)); // 21
-        System.out.println(tree.sum(bit, 6)); // 28
+        BinaryIndexedTree tree = new BinaryIndexedTree(input);
+        System.out.println(tree.sum(0)); // 1
+        System.out.println(tree.sum(1)); // 3
+        System.out.println(tree.sum(2)); // 6
+        System.out.println(tree.sum(3)); // 10
+        System.out.println(tree.sum(4)); // 15
+        System.out.println(tree.sum(5)); // 21
+        System.out.println(tree.sum(6)); // 28
         
-        // Lets do an update
         // input[3] = 6 | change = (newValue - oldValue) = (6 - 4) = 2
         // which makes, input = {1, 2, 3, 6, 5, 6, 7} and
         // bit = {0, 1, 3, 3, 12, 5, 11, 7}
-        tree.update(bit, 2, 3); 
+        tree.update(2, 3); 
         
-        System.out.println(tree.sum(bit, 3)); // 12
-        System.out.println(tree.sum(bit, 6)); // 30
+        System.out.println(tree.sum(3)); // 12
+        System.out.println(tree.sum(6)); // 30
     }
 
 }
